f6b6e8ff0f3e004c38de0362687ad44a4ca65b65
repalova-olga <norforw@gmail.com>
2024-10-09 17:17:17 +0300

    task2 and task3 added

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,9 @@
 function slugify(title) {
   const slugArr = title.split(" ");
-  return slugArr;
+  const slugStr = slugArr.join("-");
+  return slugStr.toLowerCase();
 }
 console.log(slugify("Arrays for beginners"));
+console.log(slugify("English for developer"));
+console.log(slugify("Ten secrets of JavaScript"));
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
