236b8adfb5d119ffd84cba574aec9edfe113df11
repalova-olga <norforw@gmail.com>
2024-10-19 17:30:11 +0300

    1

--- js/task-3.js
+++ js/task-3.js
@@ -1,14 +1,63 @@
-function filterArray(numbers, value) {
-  const newArr = [];
-  for (const number of numbers) {
-    if (number > value) {
-      newArr.push(number);
+// function filterArray(numbers, value) {
+//   const newArr = [];
+//   for (const number of numbers) {
+//     if (number > value) {
+//       newArr.push(number);
+//     }
+//   }
+//   return newArr;
+// }
+// console.log(filterArray([1, 2, 3, 4, 5], 3));
+// console.log(filterArray([1, 2, 3, 4, 5], 4));
+// console.log(filterArray([1, 2, 3, 4, 5], 5));
+// console.log(filterArray([12, 24, 8, 41, 76], 38));
+// console.log(filterArray([12, 24, 8, 41, 76], 20));
+
+function getAllPropValues(propName) {
+  const products = [
+    { name: "Radar", price: 1300, quantity: 4 },
+    { name: "Scanner", price: 2700, quantity: 3 },
+    { name: "Droid", price: 400, quantity: 7 },
+    { name: "Grip", price: 1200, quantity: 9 },
+  ];
+
+  const results = [];
+
+  for (const product of products) {
+    if (product.propName) {
+      results.push(product.propName);
     }
+
+    return results;
   }
-  return newArr;
 }
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([1, 2, 3, 4, 5], 4));
-console.log(filterArray([1, 2, 3, 4, 5], 5));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+console.log(getAllPropValues("name"));
+console.log(getAllPropValues("quantity"));
+console.log(getAllPropValues("price"));
+console.log(getAllPropValues("category"));
+
+// function calculateTotalPrice(productName) {
+//   const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+//   ];
+//   let result = 0;
+//   for (const product of products) {
+//     console.log(Object.keys(product));
+//     if (Object.keys(product).includes(productName)) {
+//       result = product.price * product.quantity;
+//       return result;
+//     }
+//   }
+//   if ((result = 0)) {
+//     return `Product ${productName} not found!`;
+//   }
+// }
+
+// console.log(calculateTotalPrice("Blaster"));
+// console.log(calculateTotalPrice("Radar"));
+// console.log(calculateTotalPrice("Droid"));
+// console.log(calculateTotalPrice("Grip"));
+// console.log(calculateTotalPrice("Scanner"));
